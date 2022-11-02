f7de91c3d146f251ce94c1558d3efb64f564146d
ennuo <ajr.uiz2048@gmail.com>
2022-11-02 19:59:36 -0400

    Add support for multiple vertex types in one mesh

--- src/main/java/ufg/io/MeshExporter.java
+++ src/main/java/ufg/io/MeshExporter.java
@@ -33,6 +33,7 @@ import ufg.resources.Material.MaterialParameter;
 import ufg.structures.chunks.ResourceData;
 import ufg.structures.vertex.VertexStreamDescriptor;
 import ufg.structures.vertex.VertexStreams;
+import ufg.enums.PrimitiveType;
 import ufg.enums.VertexStreamElementUsage;
 
 public class MeshExporter {
@@ -49,6 +50,7 @@ public class MeshExporter {
 
         int numAccessors = 0;
         int numTextures = 0;
+        int numViews = 0;
 
         HashMap<String, Integer> views = new HashMap<>();
 
@@ -81,7 +83,7 @@ public class MeshExporter {
 
             this.glTf.addBufferViews(view);
 
-            int index = this.views.size();
+            int index = numViews++;
             this.views.put(name, index);
 
             return index;
@@ -135,7 +137,7 @@ public class MeshExporter {
 
             // TODO: Actually check raster state
             glMaterial.setDoubleSided(true);
-            
+
             MaterialPbrMetallicRoughness pbr = new MaterialPbrMetallicRoughness();
             glMaterial.setPbrMetallicRoughness(pbr);
 
@@ -172,96 +174,147 @@ public class MeshExporter {
             if (model == null || model.meshes.length == 0 || model.numPrims == 0) return false;
 
 
-            // First pass to get vertex data
-            ufg.structures.vertex.Mesh primary = model.meshes[0];
-            VertexStreamDescriptor decl = VertexStreams.get(primary.vertexDeclUID);
-            ufg.resources.Buffer[] buffers = new ufg.resources.Buffer[] {
-                (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[0]),
-                (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[1]),
-                (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[2]),
-                (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[3])
-            };
+            // TODO: Maybe find a better way of keeping track of stream duplicates?
+
+            int[] vertCounts = new int[model.meshes.length];
+            boolean[] hasSecondaryUVs = new boolean[model.meshes.length];
+            boolean [] hasColors = new boolean[model.meshes.length];
+
+            HashMap<Integer, Integer> streamMappings = new HashMap<>();
+            HashMap<Integer, Integer> indexMappings = new HashMap<>();
+
+            for (int i = 0; i < model.meshes.length; ++i) {
+                int baseStreamOffset = this.buffer != null ? this.buffer.length : 0;
+
+                ufg.structures.vertex.Mesh primary = model.meshes[i];
+
+                int bufferHashCode = 1;
+                for (int b = 0; b < 3; ++b)
+                    bufferHashCode = 31 * bufferHashCode + primary.vertexBufferUIDs[b];
+                
+                if (streamMappings.containsKey(bufferHashCode)) {
+                    int streamIndex = streamMappings.get(bufferHashCode);
+
+                    vertCounts[i] = vertCounts[streamIndex];
+                    hasSecondaryUVs[i] = hasSecondaryUVs[streamIndex];
+                    hasColors[i] = hasColors[streamIndex];
+
+                    this.views.put("POSITIONS_" + i, this.views.get("POSITIONS_" + streamIndex));
+                    this.views.put("NORMALS_" + i, this.views.get("NORMALS_" + streamIndex));
+                    this.views.put("TANGENTS_" + i, this.views.get("TANGENTS_" + streamIndex));
+                    this.views.put("TEXCOORD0_" + i, this.views.get("TEXCOORD0_" + streamIndex));
+
+                    if (hasSecondaryUVs[streamIndex])
+                        this.views.put("TEXCOORD1_" + i, this.views.get("TEXCOORD1_" + streamIndex));
+                    if (hasColors[streamIndex])
+                        this.views.put("COLOR_" + i, this.views.get("COLOR_" + streamIndex));
+
+                    if (model.meshes[streamIndex].indexBufferUID != model.meshes[i].indexBufferUID)
+                        throw new RuntimeException("fuck off");
+                    
+                    this.views.put("INDICES_" + i, this.views.get("INDICES_" + streamIndex));
+                    
+                    continue;
+                } else streamMappings.put(bufferHashCode, i);
+
+                VertexStreamDescriptor decl = VertexStreams.get(primary.vertexDeclUID);
+                ufg.resources.Buffer[] buffers = new ufg.resources.Buffer[] {
+                    (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[0]),
+                    (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[1]),
+                    (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[2]),
+                    (ufg.resources.Buffer) this.modelStreamingResources.get(primary.vertexBufferUIDs[3])
+                };
 
 
-            boolean hasColor = decl.hasElement(VertexStreamElementUsage.COLOR0);
-            boolean hasUV1 = decl.hasElement(VertexStreamElementUsage.TEXCOORD1);
+                boolean hasColor = decl.hasElement(VertexStreamElementUsage.COLOR0);
+                boolean hasUV1 = decl.hasElement(VertexStreamElementUsage.TEXCOORD1);
 
-            int vertCount = buffers[0].numElements;
-            int indexCount = model.numPrims * 0x3;
+                int vertCount = buffers[0].numElements;
+                int indexCount = model.numPrims * 0x3;
 
-            MemoryOutputStream stream = new MemoryOutputStream(
-                (vertCount * 0xC) + // Positions
-                (vertCount * 0xC) + // Normals
-                (vertCount * 0x10) + // Tangents
-                (vertCount * (hasUV1 ? 0x10 : 0x8)) + // UV0/UV1
-                (vertCount * (hasColor ? 0x10 : 0)) + // Color
-                (indexCount * 0x2) // Indices
-            );
+                vertCounts[i] = vertCount;
+                hasSecondaryUVs[i] = hasUV1;
+                hasColors[i] = hasColor;
 
-            stream.setLittleEndian(true);
+                MemoryOutputStream stream = new MemoryOutputStream(
+                    (vertCount * 0xC) + // Positions
+                    (vertCount * 0xC) + // Normals
+                    (vertCount * 0x10) + // Tangents
+                    (vertCount * (hasUV1 ? 0x10 : 0x8)) + // UV0/UV1
+                    (vertCount * (hasColor ? 0x10 : 0)) + // Color
+                    (indexCount * 0x2) // Indices
+                );
 
-            this.addBufferView("POSITIONS", stream.getOffset(), 0xC * vertCount);
-            int POSITIONS = this.addAccessor("POSITIONS", 5126, "VEC3", 0, vertCount);
-            Vector4f[] vertices = decl.get(buffers, VertexStreamElementUsage.POSITION);
-            for (Vector4f vertex : vertices) {
-                stream.f32(vertex.x);
-                stream.f32(vertex.y);
-                stream.f32(vertex.z);
-            }
+                stream.setLittleEndian(true);
 
-            this.addBufferView("NORMALS", stream.getOffset(), 0xC * vertCount);
-            int NORMALS = this.addAccessor("NORMALS", 5126, "VEC3", 0, vertCount);
-            Vector4f[] normals = decl.get(buffers, VertexStreamElementUsage.NORMAL);
-            for (Vector4f vertex : normals) {
-                stream.f32(vertex.x);
-                stream.f32(vertex.y);
-                stream.f32(vertex.z);
-            }
-            
-            this.addBufferView("TANGENTS", stream.getOffset(), 0x10 * vertCount);
-            int TANGENTS = this.addAccessor("TEXCOORD0", 5126, "VEC4", 0, vertCount);
-            Vector4f[] tangents = decl.get(buffers, VertexStreamElementUsage.TANGENT);
-            for (Vector4f vertex : tangents) {
-                stream.f32(vertex.x);
-                stream.f32(vertex.y);
-                stream.f32(vertex.z);
-                stream.f32(1.0f);
-            }
+                this.addBufferView("POSITIONS_" + i, baseStreamOffset + stream.getOffset(), 0xC * vertCount);
+                // int POSITIONS = this.addAccessor("POSITIONS", 5126, "VEC3", 0, vertCount);
+                Vector4f[] vertices = decl.get(buffers, VertexStreamElementUsage.POSITION);
+                for (Vector4f vertex : vertices) {
+                    stream.f32(vertex.x);
+                    stream.f32(vertex.y);
+                    stream.f32(vertex.z);
+                }
 
-            this.addBufferView("TEXCOORD0", stream.getOffset(), 0x8 * vertCount);
-            int UV0 = this.addAccessor("TEXCOORD0", 5126, "VEC2", 0, vertCount);
-            Vector4f[] attributes = decl.get(buffers, VertexStreamElementUsage.TEXCOORD0);
-            for (Vector4f vertex : attributes) {
-                stream.f32(vertex.x);
-                stream.f32(vertex.y);
-            }
+                this.addBufferView("NORMALS_" + i, baseStreamOffset + stream.getOffset(), 0xC * vertCount);
+                // int NORMALS = this.addAccessor("NORMALS", 5126, "VEC3", 0, vertCount);
+                Vector4f[] normals = decl.get(buffers, VertexStreamElementUsage.NORMAL);
+                for (Vector4f vertex : normals) {
+                    stream.f32(vertex.x);
+                    stream.f32(vertex.y);
+                    stream.f32(vertex.z);
+                }
+                
+                this.addBufferView("TANGENTS_" + i, baseStreamOffset + stream.getOffset(), 0x10 * vertCount);
+                // int TANGENTS = this.addAccessor("TEXCOORD0", 5126, "VEC4", 0, vertCount);
+                Vector4f[] tangents = decl.get(buffers, VertexStreamElementUsage.TANGENT);
+                for (Vector4f vertex : tangents) {
+                    stream.f32(vertex.x);
+                    stream.f32(vertex.y);
+                    stream.f32(vertex.z);
+                    stream.f32(1.0f);
+                }
 
-            int UV1 = -1;
-            if (hasUV1) {
-                this.addBufferView("TEXCOORD1", stream.getOffset(), 0x8 * vertCount);
-                UV1 = this.addAccessor("TEXCOORD1", 5126, "VEC2", 0, vertCount);
-                Vector4f[] attribs = decl.get(buffers, VertexStreamElementUsage.TEXCOORD1);
-                for (Vector4f vertex : attribs) {
+                this.addBufferView("TEXCOORD0_" + i, baseStreamOffset + stream.getOffset(), 0x8 * vertCount);
+                // int UV0 = this.addAccessor("TEXCOORD0", 5126, "VEC2", 0, vertCount);
+                Vector4f[] attributes = decl.get(buffers, VertexStreamElementUsage.TEXCOORD0);
+                for (Vector4f vertex : attributes) {
                     stream.f32(vertex.x);
                     stream.f32(vertex.y);
                 }
-            }
 
-            int COLOR = -1;
-            if (hasColor) {
-                this.addBufferView("COLOR", stream.getOffset(), 0x10 * vertCount);
-                COLOR = this.addAccessor("COLOR", 5126, "VEC4", 0, vertCount);
-                Vector4f[] colors = decl.get(buffers, VertexStreamElementUsage.COLOR0);
-                for (Vector4f vertex : colors)
-                    stream.v4(vertex);
-            }
+                if (hasUV1) {
+                    this.addBufferView("TEXCOORD1_" + i, baseStreamOffset + stream.getOffset(), 0x8 * vertCount);
+                    // UV1 = this.addAccessor("TEXCOORD1", 5126, "VEC2", 0, vertCount);
+                    Vector4f[] attribs = decl.get(buffers, VertexStreamElementUsage.TEXCOORD1);
+                    for (Vector4f vertex : attribs) {
+                        stream.f32(vertex.x);
+                        stream.f32(vertex.y);
+                    }
+                }
 
-            this.addBufferView("INDICES", stream.getOffset(), 0x2 * indexCount);
-            MemoryInputStream indexStream = new MemoryInputStream(((ufg.resources.Buffer) this.modelStreamingResources.get(primary.indexBufferUID)).data);
-            for (int i = 0; i < indexCount; ++i)
-                stream.i16(indexStream.i16());
-            
-            this.buffer = stream.getBuffer();
+                if (hasColor) {
+                    this.addBufferView("COLOR_" + i, baseStreamOffset + stream.getOffset(), 0x10 * vertCount);
+                    // COLOR = this.addAccessor("COLOR", 5126, "VEC4", 0, vertCount);
+                    Vector4f[] colors = decl.get(buffers, VertexStreamElementUsage.COLOR0);
+                    for (Vector4f vertex : colors)
+                        stream.v4(vertex);
+                }
+
+                if (indexMappings.containsKey(primary.indexBufferUID)) {
+                    this.views.put("INDICES_" + i, indexMappings.get(primary.indexBufferUID));
+                } else {
+                    indexMappings.put(primary.indexBufferUID, this.addBufferView("INDICES_" + i, baseStreamOffset + stream.getOffset(), 0x2 * indexCount));
+                    MemoryInputStream indexStream = new MemoryInputStream(((ufg.resources.Buffer) this.modelStreamingResources.get(primary.indexBufferUID)).data);
+                    for (int j = 0; j < indexCount; ++j)
+                        stream.u16(indexStream.u16());
+                }
+
+                if (this.buffer != null)
+                    this.buffer = Bytes.combine(this.buffer, stream.getBuffer());
+                else
+                    this.buffer = stream.getBuffer();
+            }
 
             Asset asset = new Asset();
             asset.setGenerator("UFG/IO v2.0");
@@ -271,20 +324,25 @@ public class MeshExporter {
             Mesh glMesh = new Mesh();
             glMesh.setName(model.name + ".MESH");
 
+            int index = 0;
             for (ufg.structures.vertex.Mesh mesh : model.meshes) {
+                int vertCount = vertCounts[index];
+
                 MeshPrimitive primitive = new MeshPrimitive();
-                primitive.addAttributes("POSITION", POSITIONS);
-                primitive.addAttributes("NORMAL", NORMALS);
-                primitive.addAttributes("TANGENT", TANGENTS);
-                primitive.addAttributes("TEXCOORD_0", UV0);
-                if (hasUV1)
-                    primitive.addAttributes("TEXCOORD_1", UV1);
-                if (hasColor)
-                    primitive.addAttributes("COLOR_0", COLOR);
-                primitive.setIndices(this.addAccessor("INDICES", 5123, "SCALAR", mesh.indexStart * 0x2, mesh.numPrimitives * 0x3));
+                primitive.addAttributes("POSITION", this.addAccessor("POSITIONS_" + index, 5126, "VEC3", 0, vertCount));
+                primitive.addAttributes("NORMAL", this.addAccessor("NORMALS_" + index, 5126, "VEC3", 0, vertCount));
+                primitive.addAttributes("TANGENT", this.addAccessor("TANGENTS_" + index, 5126, "VEC4", 0, vertCount));
+                primitive.addAttributes("TEXCOORD_0", this.addAccessor("TEXCOORD0_" + index, 5126, "VEC2", 0, vertCount));
+                if (hasSecondaryUVs[index])
+                    primitive.addAttributes("TEXCOORD_1", this.addAccessor("TEXCOORD1_" + index, 5126, "VEC2", 0, vertCount));
+                if (hasColors[index])
+                    primitive.addAttributes("COLOR_0", this.addAccessor("COLOR_" + index, 5126, "VEC4", 0, vertCount));
+                primitive.setIndices(this.addAccessor("INDICES_" + index, 5123, "SCALAR", mesh.indexStart * 0x2, mesh.numPrimitives * 0x3));
 
                 primitive.setMaterial(this.addMaterial((ufg.resources.Material) this.modelStreamingResources.get(mesh.materialUID)));
                 glMesh.addPrimitives(primitive);
+
+                index++;
             }
 
             this.glTf.addMeshes(glMesh);
